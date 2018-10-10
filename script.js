c439f1b9979e6257237e7282ffcbc8210934a64a
Farihah Kabir <nadahkabir@gmail.com>
2018-10-10 18:28:10 +0000

    DOM Manipulation task 1

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
 /* Task 1 */
-
+var heading = document.getElementById('heading');       
+console.log(heading.innerText);
+// heading.innerText = "Text changed";
+heading.innerHTML = "<h3> Text changed </h3>";
 
 /* Task 2 using plain JavaScript */
+//Task 2a:
+var textBtn = document.getElementById('addText2');
+textBtn.addEventListener('click', function(event){
+    var task2 = document.getElementById('task2a');
+    var paragraph = document.createElement('p');
+    paragraph.innerText = "Hello World";
+    task2.appendChild(paragraph);
+});
 
+//Task 2b:
+function changeBGcolor(event){
+    var body = document.getElementsByTagName('body')[0];
+    body.style.backgroundColor = event.target.innerText.toLowerCase();
+    
+    //Another way of doing it:
+    // if(event.target.innerText == "Red"){
+    //     body.style.backgroundColor = 'red';
+    // }
+    // if(event.target.innerText == "Green"){
+    //     body.style.backgroundColor = 'green';
+    // }
+}
 
 
 /* Task 4 using jQuery */
